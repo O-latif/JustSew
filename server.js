1c287887f2cc728b8ccd351be5ff889af89a07e4
PFE-KaraMostefa-Ouadah <104235957+PFE-KaraMostefa-Ouadah@users.noreply.github.com>
2022-06-12 18:30:49 +0100

    cc

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
 const expressLayout = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const url = 'mongodb://127.0.0.1:27017';//ki tbadlah ma t7otahch f github i'll wait for you kana allah i 3awnik
+mongoose.connect(url ,{useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => app.listen(process.env.PORT || 5000))
+    .catch((err) => console.log(err));
 
 app.set('view engine' , 'ejs');
 app.set('views', __dirname + '/views');
 app.set('layout' , 'layouts/layout'); // possible delete 
 app.use(expressLayout);
 app.use(express.static("public"));
-app.listen(process.env.PORT || 3000);
+
+const homeRouter = require("./routes/home");
+
+
+
+
+
+
+
+
+app.use("/" , homeRouter);
